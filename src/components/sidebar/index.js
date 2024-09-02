eab732921ce4dc410ce7f9076356dabbbc86dc0f
krishvaghasiya705 <kvaghasiya705@gmail.com>
2024-09-02 10:09:42 +0530

    scroll navigation added and scroll indicator added

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -23,20 +23,37 @@ export default function Sidebar({ isSidebarOpen, onClose }) {
         </div>
         <div className="sidebar-content-main">
           <div className="sidebar-dropdowns-content-box-main">
-            <div className={`sidebar-dropdowns-content-box-background ${activeDropdown === 1 ? "active" : ""}`}>
-              <div className={`sidebar-dropdowns-content-box ${activeDropdown === 1 ? "active" : ""}`} onClick={() => handleDropdownClick(1)}>
-                <Link to={"/"} onClick={onClose}>About</Link>
-                <div className={`dropdown-icon ${activeDropdown === 1 ? "active" : ""}`}>
+            <div
+              className={`sidebar-dropdowns-content-box-background ${
+                activeDropdown === 1 ? "active" : ""
+              }`}
+            >
+              <div
+                className={`sidebar-dropdowns-content-box ${
+                  activeDropdown === 1 ? "active" : ""
+                }`}
+                onClick={() => handleDropdownClick(1)}
+              >
+                <p> About</p>
+                <div
+                  className={`dropdown-icon ${
+                    activeDropdown === 1 ? "active" : ""
+                  }`}
+                >
                   <Asidedropdown />
                 </div>
               </div>
               {activeDropdown === 1 && (
                 <div className="dropdown-content-sidebar-details">
                   <div className="dropdowns-links-main">
-                    <Link to={"/whyforexvoyager"} onClick={onClose}>Why Forex Voyager Limited</Link>
+                    <Link to={"/whyforexvoyager"} onClick={onClose}>
+                      Why Forex Voyager Limited
+                    </Link>
                   </div>
                   <div className="dropdowns-links-main">
-                    <Link to={"/regulations"} onClick={onClose}>Regulations</Link>
+                    <Link to={"/regulations"} onClick={onClose}>
+                      Regulations
+                    </Link>
                   </div>
                 </div>
               )}
@@ -44,32 +61,57 @@ export default function Sidebar({ isSidebarOpen, onClose }) {
           </div>
 
           <div className="sidebar-dropdowns-content-box-main">
-            <div className={`sidebar-dropdowns-content-box-background ${activeDropdown === 2 ? "active" : ""}`}>
-              <div className={`sidebar-dropdowns-content-box ${activeDropdown === 2 ? "active" : ""}`} onClick={() => handleDropdownClick(2)}>
-                <Link to={"/"} onClick={onClose}>Products</Link>
-                <div className={`dropdown-icon ${activeDropdown === 2 ? "active" : ""}`}>
+            <div
+              className={`sidebar-dropdowns-content-box-background ${
+                activeDropdown === 2 ? "active" : ""
+              }`}
+            >
+              <div
+                className={`sidebar-dropdowns-content-box ${
+                  activeDropdown === 2 ? "active" : ""
+                }`}
+                onClick={() => handleDropdownClick(2)}
+              >
+                <p>Products</p>
+                <div
+                  className={`dropdown-icon ${
+                    activeDropdown === 2 ? "active" : ""
+                  }`}
+                >
                   <Asidedropdown />
                 </div>
               </div>
               {activeDropdown === 2 && (
                 <div className="dropdown-content-sidebar-details">
                   <div className="dropdowns-links-main">
-                    <Link to={"/Forex"} onClick={onClose}>Forex</Link>
+                    <Link to={"/Forex"} onClick={onClose}>
+                      Forex
+                    </Link>
                   </div>
                   <div className="dropdowns-links-main">
-                    <Link to={"/metals"} onClick={onClose}>Metals</Link>
+                    <Link to={"/metals"} onClick={onClose}>
+                      Metals
+                    </Link>
                   </div>
                   <div className="dropdowns-links-main">
-                    <Link to={"/shares"} onClick={onClose}>Shares</Link>
+                    <Link to={"/shares"} onClick={onClose}>
+                      Shares
+                    </Link>
                   </div>
                   <div className="dropdowns-links-main">
-                    <Link to={"/indices"} onClick={onClose}>Indices</Link>
+                    <Link to={"/indices"} onClick={onClose}>
+                      Indices
+                    </Link>
                   </div>
                   <div className="dropdowns-links-main">
-                    <Link to={"/commodities"} onClick={onClose}>Commodities</Link>
+                    <Link to={"/commodities"} onClick={onClose}>
+                      Commodities
+                    </Link>
                   </div>
                   <div className="dropdowns-links-main">
-                    <Link to={"/cryptocurrencies"} onClick={onClose}>Cryptocurrencies</Link>
+                    <Link to={"/cryptocurrencies"} onClick={onClose}>
+                      Cryptocurrencies
+                    </Link>
                   </div>
                 </div>
               )}
@@ -77,20 +119,37 @@ export default function Sidebar({ isSidebarOpen, onClose }) {
           </div>
 
           <div className="sidebar-dropdowns-content-box-main">
-            <div className={`sidebar-dropdowns-content-box-background ${activeDropdown === 3 ? "active" : ""}`}>
-              <div className={`sidebar-dropdowns-content-box ${activeDropdown === 3 ? "active" : ""}`} onClick={() => handleDropdownClick(3)}>
-                <Link to={"/"} onClick={onClose}>Platforms</Link>
-                <div className={`dropdown-icon ${activeDropdown === 3 ? "active" : ""}`}>
+            <div
+              className={`sidebar-dropdowns-content-box-background ${
+                activeDropdown === 3 ? "active" : ""
+              }`}
+            >
+              <div
+                className={`sidebar-dropdowns-content-box ${
+                  activeDropdown === 3 ? "active" : ""
+                }`}
+                onClick={() => handleDropdownClick(3)}
+              >
+                <p>Platforms</p>
+                <div
+                  className={`dropdown-icon ${
+                    activeDropdown === 3 ? "active" : ""
+                  }`}
+                >
                   <Asidedropdown />
                 </div>
               </div>
               {activeDropdown === 3 && (
                 <div className="dropdown-content-sidebar-details">
                   <div className="dropdowns-links-main">
-                    <Link to={"/meta5"} onClick={onClose}>MT5 Platform</Link>
+                    <Link to={"/meta5"} onClick={onClose}>
+                      MT5 Platform
+                    </Link>
                   </div>
                   <div className="dropdowns-links-main">
-                    <Link to={"/webtradermeta5"} onClick={onClose}>Web Trader MT5</Link>
+                    <Link to={"/webtradermeta5"} onClick={onClose}>
+                      Web Trader MT5
+                    </Link>
                   </div>
                 </div>
               )}
@@ -98,26 +157,47 @@ export default function Sidebar({ isSidebarOpen, onClose }) {
           </div>
 
           <div className="sidebar-dropdowns-content-box-main">
-            <div className={`sidebar-dropdowns-content-box-background ${activeDropdown === 4 ? "active" : ""}`}>
-              <div className={`sidebar-dropdowns-content-box ${activeDropdown === 4 ? "active" : ""}`} onClick={() => handleDropdownClick(4)}>
-                <Link to={"/"} onClick={onClose}>Accounts</Link>
-                <div className={`dropdown-icon ${activeDropdown === 4 ? "active" : ""}`}>
+            <div
+              className={`sidebar-dropdowns-content-box-background ${
+                activeDropdown === 4 ? "active" : ""
+              }`}
+            >
+              <div
+                className={`sidebar-dropdowns-content-box ${
+                  activeDropdown === 4 ? "active" : ""
+                }`}
+                onClick={() => handleDropdownClick(4)}
+              >
+                <p>Accounts</p>
+                <div
+                  className={`dropdown-icon ${
+                    activeDropdown === 4 ? "active" : ""
+                  }`}
+                >
                   <Asidedropdown />
                 </div>
               </div>
               {activeDropdown === 4 && (
                 <div className="dropdown-content-sidebar-details">
                   <div className="dropdowns-links-main">
-                    <Link to={"/"} onClick={onClose}>Compare Accounts</Link>
+                    <Link to={"/"} onClick={onClose}>
+                      Compare Accounts
+                    </Link>
                   </div>
                   <div className="dropdowns-links-main">
-                    <Link to={"/Standard"} onClick={onClose}>Standard</Link>
+                    <Link to={"/Standard"} onClick={onClose}>
+                      Standard
+                    </Link>
                   </div>
                   <div className="dropdowns-links-main">
-                    <Link to={"/pro"} onClick={onClose}>Pro</Link>
+                    <Link to={"/pro"} onClick={onClose}>
+                      Pro
+                    </Link>
                   </div>
                   <div className="dropdowns-links-main">
-                    <Link to={"/ecn"} onClick={onClose}>ECN</Link>
+                    <Link to={"/ecn"} onClick={onClose}>
+                      ECN
+                    </Link>
                   </div>
                 </div>
               )}
@@ -125,20 +205,37 @@ export default function Sidebar({ isSidebarOpen, onClose }) {
           </div>
 
           <div className="sidebar-dropdowns-content-box-main sidebar-dropdowns-content-box-main-sc">
-            <div className={`sidebar-dropdowns-content-box-background ${activeDropdown === 7 ? "active" : ""}`}>
-              <div className={`sidebar-dropdowns-content-box ${activeDropdown === 7 ? "active" : ""}`} onClick={() => handleDropdownClick(7)}>
-                <Link to={"/"} onClick={onClose}>Partnership</Link>
-                <div className={`dropdown-icon ${activeDropdown === 7 ? "active" : ""}`}>
+            <div
+              className={`sidebar-dropdowns-content-box-background ${
+                activeDropdown === 7 ? "active" : ""
+              }`}
+            >
+              <div
+                className={`sidebar-dropdowns-content-box ${
+                  activeDropdown === 7 ? "active" : ""
+                }`}
+                onClick={() => handleDropdownClick(7)}
+              >
+                <p>Partnership</p>
+                <div
+                  className={`dropdown-icon ${
+                    activeDropdown === 7 ? "active" : ""
+                  }`}
+                >
                   <Asidedropdown />
                 </div>
               </div>
               {activeDropdown === 7 && (
                 <div className="dropdown-content-sidebar-details">
                   <div className="dropdowns-links-main">
-                    <Link to={"/"} onClick={onClose}>Introducing Brokers</Link>
+                    <Link to={"/"} onClick={onClose}>
+                      Introducing Brokers
+                    </Link>
                   </div>
                   <div className="dropdowns-links-main">
-                    <Link to={"/"} onClick={onClose}>Franchise Program</Link>
+                    <Link to={"/"} onClick={onClose}>
+                      Franchise Program
+                    </Link>
                   </div>
                 </div>
               )}
@@ -165,7 +262,12 @@ export default function Sidebar({ isSidebarOpen, onClose }) {
           </div>
         </div>
       </div>
-      <div className={`sidebarbackground ${isSidebarOpen ? "sidebarbackgroundopen" : ""}`} onClick={onClose}></div>
+      <div
+        className={`sidebarbackground ${
+          isSidebarOpen ? "sidebarbackgroundopen" : ""
+        }`}
+        onClick={onClose}
+      ></div>
     </>
   );
 }
